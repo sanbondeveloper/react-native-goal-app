daf83229bb11e497677b85119c6f975327bc0875
sanbondeveloper <sanbondeveloper@gmail.com>
2024-07-18 13:22:16 +0900

    ✨ 컴포넌트 분리

--- App.js
+++ App.js
@@ -1,36 +1,18 @@
 import { useState } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  TextInput,
-  ScrollView,
-  FlatList,
-} from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
-  const [enteredGoalText, setEnteredGoalText] = useState('');
   const [courseGoals, setCourseGoals] = useState([]);
 
-  function goalInputHandler(enteredText) {
-    setEnteredGoalText(enteredText);
-  }
-
-  function addGoalHandler() {
+  function addGoalHandler(enteredGoalText) {
     setCourseGoals((prev) => [...prev, enteredGoalText]);
   }
 
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Your course goal!"
-          onChangeText={goalInputHandler}
-        />
-        <Button title="Add Goal" onPress={addGoalHandler} />
-      </View>
+      <GoalInput onAddGoal={addGoalHandler} />
       <View style={styles.goalsContainer}>
         {/* <ScrollView alwaysBounceVertical={false}>
           {courseGoals.map((goal, index) => (
@@ -42,11 +24,7 @@ export default function App() {
         <FlatList
           data={courseGoals}
           renderItem={(itemData) => {
-            return (
-              <View style={styles.goalItem}>
-                <Text style={styles.goalText}>{itemData.item}</Text>
-              </View>
-            );
+            return <GoalItem text={itemData.item} />;
           }}
           keyExtractor={(item, index) => index}
           alwaysBounceVertical={false}
@@ -56,42 +34,14 @@ export default function App() {
   );
 }
 
-// 부모 속성이 자식 속성으로 상속되지 않는다.
-// 버튼에는 스타일을 지정할 수 없다.
-// 안드로이드와 iOS간 스타일링의 차이점이 있을 수 있다.
-// ScrollView는 전체 UI가 렌더링될 때마다 안에 있는 항목을 전부 렌더링합니다. 따라서 분량이 제한된 콘텐츠에 적합
 const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
     paddingTop: 50,
     paddingHorizontal: 16,
   },
-  inputContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    width: '70%',
-    marginRight: 8,
-    padding: 8,
-  },
+
   goalsContainer: {
     flex: 5,
-  },
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: '#5e0acc',
-  },
-  goalText: {
-    color: '#fff',
   },
 });
